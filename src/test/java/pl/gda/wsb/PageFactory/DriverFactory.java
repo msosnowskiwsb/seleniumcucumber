66e6eb23d26b8347ed36ee2bb633c5251e4ebf59
Mateusz Sosnowski <msosnowski@wsb.gda.pl>
2021-01-19 23:47:54 +0100

    Add DriverFactory

---
+++ src/test/java/pl/gda/wsb/PageFactory/DriverFactory.java
@@ -0,0 +1,30 @@
+package pl.gda.wsb.PageFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory {
+
+    private static WebDriver driver;
+
+    private DriverFactory(){
+        // instancja
+    }
+
+    @BeforeSuite
+    public static WebDriver getChromeDriver(){
+        if(driver == null){
+            System.setProperty("webdriver.chrome.driver","src\\test\\resources\\drivers\\chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().window().fullscreen();
+        }
+        return driver;
+    }
+}
